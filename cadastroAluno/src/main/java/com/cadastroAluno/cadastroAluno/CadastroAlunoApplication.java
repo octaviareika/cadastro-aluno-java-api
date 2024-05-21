b6a8608fa2faac379c3967004a05d7af5d5c184b
octaviareika <ceciliaperet81@gmail.com>
2024-05-21 17:59:33 -0300

    Inicializando projeto

---
+++ cadastroAluno/src/main/java/com/cadastroAluno/cadastroAluno/CadastroAlunoApplication.java
@@ -0,0 +1,13 @@
+package com.cadastroAluno.cadastroAluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAlunoApplication.class, args);
+	}
+
+}
