e8784158f8fa45b970d829a585fd5fa6a1ce9b82
octaviareika <ceciliaperet81@gmail.com>
2024-05-21 18:01:04 -0300

    Inicializando projeto

--- cadastroAluno/src/main/java/com/cadastroAluno/cadastroAluno/CadastroAlunoApplication.java
+++ cadastroAluno/src/main/java/com/cadastroAluno/cadastroAluno/CadastroAlunoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastroAlunoApplication {
+public class CadastroAlunoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeiculoApiApplication.class, args);
