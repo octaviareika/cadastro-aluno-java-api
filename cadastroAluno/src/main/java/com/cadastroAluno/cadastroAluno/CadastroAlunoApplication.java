d8603b75cdc57d7f2df031ca3a88db243570f269
BrunoSoaresV <brunoverissimo398@gmail.com>
2024-05-21 20:53:11 -0300

    teste

--- cadastroAluno/src/main/java/com/cadastroAluno/cadastroAluno/CadastroAlunoApplication.java
+++ cadastroAluno/src/main/java/com/cadastroAluno/cadastroAluno/CadastroAlunoApplication.java
@@ -9,7 +9,6 @@ public class CadastroAlunoApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(VeiculoApiApplication.class, args);
 	}
-
 	public void run(String... args) throws IOException, InterruptedException {
 		System.out.println("Hello, Spring Boot!");
 		// Principal principal = new Principal();
